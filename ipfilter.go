efbf03c9c24405650a0b2d042353834fc01fb806
Sebastian Ehmann <sebastian@dipolmoment.de>
2020-08-26 15:14:11 +0200

    Add IPFilter

---
+++ ipfilter.go
@@ -0,0 +1,110 @@
+package middleware
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+)
+
+type ipFilter struct {
+	next    http.Handler
+	permittedNets []* net.IPNet
+	ipHeader  string
+}
+
+func (ipf ipFilter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	var ip string
+	if ipf.ipHeader != "" {
+		ip = r.Header.Get(ipf.ipHeader)
+	} else {
+		ip = r.RemoteAddr
+	}
+	isPermittedIP, err := isPermittedIP(ip, ipf.permittedNets)
+	if err == nil && isPermittedIP {
+		ipf.next.ServeHTTP(w, r)
+	} else {
+		log.Printf("IP %s is not permitted to access %s \n", ip, r.URL )
+	}
+
+}
+
+func isPermittedIP(remoteIP string, permittedNets []*net.IPNet) (bool, error) {
+	ip := getIpFromString(remoteIP)
+	if ip == nil {
+		return false, fmt.Errorf("Invalid IP: %s", remoteIP)
+	}
+	for _, prmn := range permittedNets {
+		fmt.Println(prmn)
+		if prmn.Contains(ip){
+			return true, nil
+		}
+	}
+	return false, nil
+
+}
+
+func IPFilter(ipRanges []string, header string) func(http.Handler) http.Handler {
+	permittedNets, err := convertToIPNet(ipRanges)
+	if err != nil{
+		panic(fmt.Sprintf("Failed to convert ip ranges %s", err))
+	}
+	fn := func(next http.Handler) http.Handler {
+		return ipFilter{next, permittedNets, header}
+	}
+	return fn
+}
+
+func convertToIPNet(ipRanges []string) ([]*net.IPNet, error) {
+	var ipNets []*net.IPNet
+	for _, ipr := range ipRanges{
+		// allow special values for localhost
+		if ipr == "localhost" || ipr == "loopback"{
+			ipr = "::1/128"
+		}
+		// allow single IP addresses without CIDR suffix
+		if strings.Index(ipr, "/") == -1{
+			if strings.Index(ipr, ".") !=-1 { //assume IPv4
+				ipr += "/32"
+			} else if strings.Index(ipr, ":") != -1 { //assume IPv6
+				ipr += "/128"
+			} else {
+				return nil, fmt.Errorf("invalid ip: %s", ipr)
+			}
+		}
+		_, ipNet, err := net.ParseCIDR(ipr)
+		if err!=nil{
+			return nil, err
+		}
+
+		if  ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil { // Add IPv6 loopback if permitted range is IPv4 loopback
+				loopbackIPv6 := net.IPNet{IP: net.IPv6loopback, Mask: net.CIDRMask(128, 128)}
+				ipNets = append(ipNets, &loopbackIPv6)
+			} else { // Add IPv4 loopback if permitted range is IPv6 loopback
+				loopbackIPv6 := net.IPNet{IP: net.IPv4(127, 0, 0, 0), Mask: net.CIDRMask(8, 32)}
+				ipNets = append(ipNets, &loopbackIPv6)
+			}
+		}
+		ipNets = append(ipNets, ipNet)
+	}
+	fmt.Println(ipNets)
+	return ipNets, nil
+}
+
+func getIpFromString(addr string) net.IP {
+	for _, c := range "[]\"" {
+		addr = strings.ReplaceAll(addr, string(c), "")
+	}
+	colonIndex := strings.LastIndex(addr, ":")
+	if colonIndex == -1 {
+		return nil
+	}
+	addr = addr[:strings.LastIndex(addr, ":")]
+	ip := net.ParseIP(addr)
+	return ip
+}
+
+
---
+++ ipfilter_test.go
@@ -0,0 +1,67 @@
+package middleware
+
+
+import (
+	"net"
+	"testing"
+)
+
+func Test_getIpFromString(t *testing.T) {
+
+	tests := []struct {
+		name string
+		addr string
+		want net.IP
+	}{
+		{"localhost IPv4", "127.0.0.1:1234", net.ParseIP("127.0.0.1")},
+		{"IPv4 in quotes", "\"127.0.0.1:1234\"", net.ParseIP("127.0.0.1")},
+		{"localhost IPv6", "[::1]:57048", net.ParseIP("::1")},
+		{"IPv6 in quotes", "\"[::1]\":57048", net.ParseIP("::1")},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getIpFromString(tt.addr); got.String() != tt.want.String() {
+				t.Errorf("getIpFromString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isPermittedIP(t *testing.T) {
+	type args struct {
+		remoteIP string
+		permittedNets []*net.IPNet
+	}
+	net1, _ := convertToIPNet([]string{"127.0.0.1/32"})
+	net2, _ := convertToIPNet([]string{"::1/128"})
+	net3, _ := convertToIPNet([]string{"127.0.0.1/32"})
+	net4, _ := convertToIPNet([]string{"::1/128"})
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		{"Localhost IPv4 ", args{"127.0.0.1:1234", net1}, true, false},
+		{"Localhost IPv6 ", args{"[::1]:57048", net2}, true, false},
+		{"Loopback Mix IPv6 IPv4", args{"[::1]:1234", net3 }, true, false},
+		{"Loopback Mix IPv4 IPv6", args{"127.0.0.1:12345", net4}, true, false},
+		{"Loopback Mix Permitted 1", args{"127.1.0.1:12345", net4}, true, false},
+		{"Loopback Mix Permitted 2", args{"127.255.0.1:12345", net4}, true, false},
+		{"Loopback Mix IP not permitted", args{"128.0.0.1:12345", net4}, false, false},
+		{"Loopback Mix IP not permitted", args{"126.0.0.1:12345", net4}, false, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := isPermittedIP(tt.args.remoteIP, tt.args.permittedNets)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("isPermittedIP() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("isPermittedIP() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
