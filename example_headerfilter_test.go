0c9ab25d87c1244305c883cad74ff3bfc5a067a4
Sebastian Ehmann <sebastian@dipolmoment.de>
2020-08-27 16:56:40 +0200

    Add example-test for header filter

--- example_headerfilter_test.go
+++ example_headerfilter_test.go
@@ -1,1 +1,34 @@
-package middleware
+package middleware_test
+
+import (
+	"fmt"
+	"github.com/seb-ehm/middleware"
+	"log"
+	"net/http"
+)
+
+func ExampleHeaderFilter() {
+	mux := http.NewServeMux()
+	//Some handler that prints both to stdout and the http response
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hi!")
+	}
+
+	requireHeader := middleware.FilterHeaders(map[string][]string{"mysecretkey": {"mysecretvalue"}})
+
+	authenticated := middleware.New().Append(requireHeader)
+
+	mux.Handle("/authenticated", authenticated.ApplyToFunc(handler))
+
+	go func() { log.Fatal(http.ListenAndServe("localhost:9193", mux)) }()
+
+	_, status := GetWebsiteWithHeader("http://localhost:9193/authenticated", "mysecretkey", "wrongvalue")
+	content, _ := GetWebsiteWithHeader("http://localhost:9193/authenticated", "mysecretkey", "mysecretvalue")
+
+	fmt.Println(status)
+	fmt.Println(content)
+	// Output:
+	// 403 Forbidden
+	// Hi!
+}
+
