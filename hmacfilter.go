fb7f4539a1eec27eb4fa66bee97292910756e3c0
Sebastian Ehmann <sebastian@dipolmoment.de>
2020-08-31 18:15:38 +0200

    Add failing GitHub test

--- hmacfilter.go
+++ hmacfilter.go
@@ -10,7 +10,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 	"time"
 )
@@ -21,13 +20,13 @@ type hmacFilter struct {
 }
 
 type HmacParams struct {
-	provider      string
-	secretEnv     string
-	hmacSource    string
-	nonceSource   string
-	timeSource    string
-	base64Encoded bool
-	includeURL    bool
+	Provider      string
+	Secret        string
+	HmacSource    string
+	NonceSource   string
+	TimeSource    string
+	Base64Encoded bool
+	IncludeURL    bool
 }
 
 func (hm hmacFilter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -52,79 +51,91 @@ func (hm hmacFilter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func HmacFilter(params HmacParams) func(http.Handler) http.Handler {
 	var validateFn func(*http.Request, []byte) (bool, error)
-	switch params.provider {
+	switch params.Provider {
 	case "github":
 		{
-			validateFn = func(r *http.Request, message []byte) (bool, error) {
-				messageMAC := []byte(r.Header.Get("X-Hub-Signature"))
-				if len(messageMAC) == 0 {
-					err := fmt.Errorf("missing HMAC header")
-					return false, err
-				}
-				secret, ok := os.LookupEnv(params.secretEnv)
-				if !ok {
-					err := fmt.Errorf("missing HMAC enironment variable")
-					return false, err
-				}
-				mac := hmac.New(sha1.New, []byte(secret))
-				mac.Write(message)
-				expected := mac.Sum(nil)
-				return hmac.Equal(messageMAC[5:], expected), nil
-			}
+			validateFn = GithubValidation(params)
 
 		}
 	default:
 		{
-			validateFn = func(r *http.Request, message []byte) (bool, error) {
-				messageMAC := []byte(r.Header.Get(params.hmacSource))
-				secretEnv, ok := os.LookupEnv(params.secretEnv)
-				secret := []byte(secretEnv)
-				if !ok {
-					err := fmt.Errorf("missing HMAC enironment variable")
-					return false, err
-				}
-				var err error
-				if params.base64Encoded {
-					secret, err = base64.StdEncoding.DecodeString(string(secret))
-					if err != nil {
-						return false, fmt.Errorf("invalid HMAC in environment variable %s: %w", params.secretEnv, err)
-					}
-					messageMAC, err = base64.StdEncoding.DecodeString(string(messageMAC))
-					if err != nil {
-						return false, fmt.Errorf("invalid signature in header %s: %w", params.hmacSource, err)
-					}
-				}
-
-				mac := hmac.New(sha256.New, secret)
-				if params.includeURL {
-					mac.Write([]byte(r.URL.String()))
-				}
-				if params.nonceSource != "" {
-					mac.Write([]byte(r.Header.Get(params.nonceSource)))
-				}
-
-				if params.timeSource != "" {
-					timestamp, err := strconv.ParseInt(r.Header.Get(params.timeSource), 10, 64)
-					if err != nil {
-						return false, fmt.Errorf("error in timestamp from header %s: %w", params.timeSource, err)
-					}
-					sendTime := time.Unix(timestamp, 0)
-					receiveTime := time.Now()
-					if receiveTime.Sub(sendTime) > time.Second*2 {
-						return false, nil
-					}
-				}
-
-				expected := mac.Sum(nil)
-				return hmac.Equal(messageMAC, expected), nil
-
-			}
+			validateFn = DefaultValidation(params)
 		}
 	}
 	fn := func(next http.Handler) http.Handler {
 		return hmacFilter{next, validateFn}
 	}
 	return fn
+}
+
+func DefaultValidation(params HmacParams) func(r *http.Request, message []byte) (bool, error) {
+	return func(r *http.Request, message []byte) (bool, error) {
+		messageMAC := []byte(r.Header.Get(params.HmacSource))
+		if len(params.Secret) == 0 {
+			err := fmt.Errorf("empty HMAC secret")
+			return false, err
+		}
+		secret := []byte(params.Secret)
+
+		var err error
+		if params.Base64Encoded {
+			secret, err = base64.StdEncoding.DecodeString(string(secret))
+			if err != nil {
+				return false, fmt.Errorf("invalid secret: %w", err)
+			}
+			messageMAC, err = base64.StdEncoding.DecodeString(string(messageMAC))
+			if err != nil {
+				return false, fmt.Errorf("invalid signature in header %s: %w", params.HmacSource, err)
+			}
+		}
+
+		mac := hmac.New(sha256.New, secret)
+		if params.IncludeURL {
+			mac.Write([]byte(r.URL.String()))
+		}
+		if params.NonceSource != "" {
+			mac.Write([]byte(r.Header.Get(params.NonceSource)))
+		}
+
+		if params.TimeSource != "" {
+			timestamp, err := strconv.ParseInt(r.Header.Get(params.TimeSource), 10, 64)
+			if err != nil {
+				return false, fmt.Errorf("error in timestamp from header %s: %w", params.TimeSource, err)
+			}
+			sendTime := time.Unix(timestamp, 0)
+			receiveTime := time.Now()
+			if receiveTime.Sub(sendTime) > time.Second*2 {
+				return false, nil
+			}
+		}
+
+		expected := mac.Sum(nil)
+		return hmac.Equal(messageMAC, expected), nil
+
+	}
+}
+
+func GithubValidation(params HmacParams) func(r *http.Request, message []byte) (bool, error) {
+	return func(r *http.Request, message []byte) (bool, error) {
+		messageMAC := []byte(r.Header.Get("X-Hub-Signature"))
+		if len(messageMAC) == 0 {
+			err := fmt.Errorf("missing HMAC header")
+			return false, err
+		}
+
+		if len(params.Secret) == 0 {
+			err := fmt.Errorf("empty HMAC secret")
+			return false, err
+		}
+		mac := hmac.New(sha1.New, []byte(params.Secret))
+		mac.Write(message)
+		expected := mac.Sum(nil)
+		test := messageMAC[5:]
+		_ = test
+		test2,_ :=  base64.StdEncoding.DecodeString(string(test))
+		_ = test2
+		return hmac.Equal(messageMAC[5:], expected), nil
+	}
 }
 
 func ValidMAC(message, messageMAC, key []byte) bool {
