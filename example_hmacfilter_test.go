967131f969ebb764805237351844d7c9c256353f
Sebastian Ehmann <sebastian@dipolmoment.de>
2020-08-31 22:26:34 +0200

    Add test with invalid request signature

--- example_hmacfilter_test.go
+++ example_hmacfilter_test.go
@@ -22,11 +22,17 @@ func ExampleHmacFilter() {
 
 	go func() { log.Fatal(http.ListenAndServe("localhost:9194", mux)) }()
 	client := &http.Client{}
-	request, _ := http.NewRequest("POST", "http://localhost:9194/authenticated", bytes.NewBuffer([]byte("ThisIsARequest")))
-	request.Header.Add("X-Hub-Signature", "sha1=8c08e9b7e2bdb4d87982f40d6bf6d36c0d0caab4")
-	resp, _ := client.Do(request)
-
+	validRequest, _ := http.NewRequest("POST", "http://localhost:9194/authenticated", bytes.NewBuffer([]byte("ThisIsARequest")))
+	validRequest.Header.Add("X-Hub-Signature", "sha1=8c08e9b7e2bdb4d87982f40d6bf6d36c0d0caab4")
+	resp, _ := client.Do(validRequest)
 	fmt.Println(resp.Status)
+
+	invalidRequest, _ := http.NewRequest("POST", "http://localhost:9194/authenticated", bytes.NewBuffer([]byte("ThisIsARequest")))
+	invalidRequest.Header.Add("X-Hub-Signature", "sha1=1234")
+	resp, _ = client.Do(invalidRequest)
+	fmt.Println(resp.Status)
+
 	// Output:
 	// 200 OK
+	// 403 Forbidden
 }
